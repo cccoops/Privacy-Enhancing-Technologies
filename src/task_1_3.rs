d09776d630b7cb1f5e02bae995fcc88c49b70ac4
Radek Sedlacek <xsedla13@mendelu.cz>
2025-11-01 17:41:50 +0100

    Taks 3 from Radek

--- src/task_1_3.rs
+++ src/task_1_3.rs
@@ -8,10 +8,14 @@ pub struct Student {
 
 impl Student {
     // Function to create a new Student instance
-    pub fn new_student(name: String, age: u32, gpa: f32) -> Student {}
+    pub fn new_student(name: String, age: u32, gpa: f32) -> Student {
+        Student { name, age, gpa}
+    }
 
     // Method to display student information
-    pub fn display(&self) {}
+    pub fn display(&self) {
+        println!("Student: {}, age: {}, gpa{}", self.name, self.age, self.gpa);
+    }
 }
 
 // Enums
@@ -23,10 +27,21 @@ pub enum TrafficLight {
 }
 
 impl TrafficLight {
+    pub fn light_duration(&self) -> u32 {
+        match self {
+            TrafficLight::Red => 3600,
+            TrafficLight::Yellow => 300,
+            TrafficLight::Green => 300,
+        }
+    }
     // Function to return the duration of each light
-    pub fn light_duration(&self) -> u32 {}
 }
-
 // Option Enum
 // Function to safely divide two integers
-pub fn safe_divide(a: i32, b: i32) -> Option<i32> {}
+pub fn safe_divide(a: i32, b: i32) -> Option<i32> {
+    if b == 0 {
+        None
+    } else {
+        Some(a / b)
+    }
+}
