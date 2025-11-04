afc355d48e4c1c92a2eb68c9882111cf12fc0885
Lukas <schroeder_lukas@icloud.com>
2025-11-04 11:21:28 +0100

    init

--- src/task_1_1.rs
+++ src/task_1_1.rs
@@ -1,11 +1,43 @@
 // Conditional Statements: max_of_three function
-pub fn max_of_three(a: i32, b: i32, c: i32) -> i32 {}
+pub fn max_of_three(a: i32, b: i32, c: i32) -> i32 {
+    if a > b {
+        if a > c { a } else { c }
+    } else {
+        if b > c { b } else { c }
+    }
+}
 
 // Loops: sum_even_numbers function
-pub fn sum_even_numbers() -> i32 {}
+pub fn sum_even_numbers() -> i32 {
+    let mut sum = 0;
+    for i in 0..=100 {
+        if i % 2 == 0 {
+            sum += i;
+        }
+    }
+    sum
+}
 
 // Loops: Countdown function with while loop
-pub fn countdown() {}
+pub fn countdown() {
+    let mut count = 10;
+    while count > 0 {
+        println!("{}", count);
+        count -= 1;
+    }
+    println!("Liftoff!");
+}
 
 // Match Statement: day_of_week function
-pub fn day_of_week(day: i32) -> &'static str {}
+pub fn day_of_week(day: i32) -> &'static str {
+    match day {
+        1 => "Monday",
+        2 => "Tuesday",
+        3 => "Wednesday",
+        4 => "Thursday",
+        5 => "Friday",
+        6 => "Saturday",
+        7 => "Sunday",
+        _ => "Invalid day",
+    }
+}
--- src/task_1_2.rs
+++ src/task_1_2.rs
@@ -1,16 +1,24 @@
 // Function: factorial (recursive)
-pub fn factorial(n: u32) -> u32 {}
+pub fn factorial(n: u32) -> u32 {
+    if n == 0 { 1 } else { n * factorial(n - 1) }
+}
 
 // Function: is_prime (using factorial function)
-pub fn is_prime(n: u32) -> bool {}
+pub fn is_prime(n: u32) -> bool {
+    (factorial(n - 1) + 1) % n == 0
+}
 
 // Ownership and Borrowing
 
 // Function: reverse_string (mutably borrow and reverse in place)
-pub fn reverse_string(s: &mut String) {}
+pub fn reverse_string(s: &mut String) {
+    *s = s.chars().rev().collect();
+}
 
 // Function: concat_strings (concatenates two &str and returns a String)
-pub fn concat_strings(s1: &str, s2: &str) -> String {}
+pub fn concat_strings(s1: &str, s2: &str) -> String {
+    format!("{}{}", s1, s2)
+}
 
 // Slices
 
